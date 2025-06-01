24c5029c25f0a7758db6a2e59c0cdec272c1567d
AmeerAYC <ameer.zeiad@gmail.com>
2025-06-01 12:14:26 +0300

    Delete send_to_telegram.py

--- send_to_telegram.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-
-def send_message(message):
-    token = os.getenv("TELEGRAM_BOT_TOKEN")
-    chat_id = os.getenv("TELEGRAM_CHAT_ID")
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    data = {"chat_id": chat_id, "text": message}
-    requests.post(url, data=data)
